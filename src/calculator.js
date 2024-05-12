dd195ddee31427b96424dc552a85d69678b179d9
Jeffry Jimenez <shino17jt@gmail.com>
2024-05-12 02:13:13 -0600

    added NY break rules

--- src/calculator.js
+++ src/calculator.js
@@ -65,9 +65,7 @@ export class BreakCalculator{
     this.GetPaidHours();
   }
 
-  GetPaidHours(){
-    const AllLocations = document.querySelector('.locations').value;
-    const totalHours = this.TimeToMilliseconds(this.savedMilliseconds);
+  AddPaidHours(timeAdded, totalHours){
 
     const box = this.totalBox;
     const boxPaid = this.totalBoxPaid;
@@ -77,50 +75,98 @@ export class BreakCalculator{
       boxPaid.innerHTML = `<p><span class="total-time">${timePassed[0]}:${timePassed[1]}</span></p>`;
     };
 
+    const totalTime = this.TimeToMilliseconds(this.savedMilliseconds - (timeAdded));
 
-    if(parseInt(+totalHours[0]) > 9 && parseInt(+totalHours[1]) >= 31 && AllLocations === "MX" || parseInt(+totalHours[0]) > 9 && parseInt(+totalHours[1]) >= 31 &&  AllLocations === "US"){
+    addTimeUI(totalTime);
 
-      const totalTime = this.TimeToMilliseconds(this.savedMilliseconds - (1800000 * 2));
+  }
 
-      addTimeUI(totalTime);
+  GetPaidHours(){
+    const AllLocations = document.querySelector('.locations').value;
+    const totalHours = this.TimeToMilliseconds(this.savedMilliseconds);
 
-      this.USMXbreakRules(totalHours);
-
-    } else if(parseInt(+totalHours[0]) > 4 && parseInt(+totalHours[1]) >= 1 && AllLocations === "MX" || parseInt(+totalHours[0]) > 4 && parseInt(+totalHours[1]) >= 1 &&  AllLocations === "US"){
-
-      const totalTime = this.TimeToMilliseconds(this.savedMilliseconds - 1800000);
-
-      addTimeUI(totalTime);
-
-      this.USMXbreakRules(totalHours);
-
-    } else if (parseInt(+totalHours[0]) > 5 && parseInt(+totalHours[1]) >= 0 && AllLocations === "MX" || parseInt(+totalHours[0]) > 5 && parseInt(+totalHours[1]) >= 0 && AllLocations === "US" ){
-
-      const totalTime = this.TimeToMilliseconds(this.savedMilliseconds - 1800000);
-
-      addTimeUI(totalTime);
-
-      this.USMXbreakRules(totalHours);
-
-    }  else if (AllLocations === "MX" || AllLocations === "US") {
-
-      const totalTime = this.TimeToMilliseconds(this.savedMilliseconds);
-
-      addTimeUI(totalTime);
-
-      this.USMXbreakRules(totalHours);
-
+    if (AllLocations === "MX" && parseInt(+totalHours[0]) > 10) {
+      this.MXBreakRules(totalHours);
     }
-     else {
 
-      const totalTime = this.TimeToMilliseconds(this.savedMilliseconds);
-
-      addTimeUI(totalHours);
-      this.CRbreakRules(totalHours);
-      
+    if (AllLocations === "NY") {
+      this.CheckNYconditions(totalHours);
     }
     
+    else if ( AllLocations === "MX" || AllLocations === "US" ) {
+      this.CheckUSMXconditions(totalHours);
+    } 
+
+    else {
+      this.AddPaidHours((0), totalHours);
+      this.CRbreakRules(totalHours);
+    }
   }
+
+  CheckUSMXconditions(totalHours){
+
+    if( ( parseInt(+totalHours[0]) > 9 && parseInt(+totalHours[1]) >= 31 ) || ( parseInt(+totalHours[0]) > 10 ) ){
+      
+      this.AddPaidHours((1800000 * 2), totalHours);
+      
+    }
+
+    else if (parseInt(+totalHours[0]) > 4 && parseInt(+totalHours[1]) >= 1) {
+      this.AddPaidHours((1800000), totalHours);
+    }
+
+    else if (parseInt(+totalHours[0]) > 5 && parseInt(+totalHours[1]) >= 0) {
+      this.AddPaidHours((1800000), totalHours);
+    }
+
+    else {
+      this.AddPaidHours((0), totalHours);
+    }
+
+    this.USMXbreakRules(totalHours);
+
+  }
+
+  CheckNYconditions(totalHours) {
+
+    const shiftStar = new Date(this.startTime.value);
+    const shiftEnd = new Date(this.endTime.value);
+
+    if ((shiftStar.getHours() > 12 && shiftEnd.getHours() < 7 ) && parseInt(+totalHours[0]) > 9 && parseInt(+totalHours[1]) >= 31  ){
+      this.AddPaidHours((2700000 + 1800000), totalHours);
+    }
+
+    else if((shiftStar.getHours() > 12 && shiftEnd.getHours() <= 23 ) && parseInt(+totalHours[0]) > 5 || (shiftStar.getHours() >= 0 && shiftEnd.getHours() < 7 ) && parseInt(+totalHours[0]) > 5 ) {
+      this.AddPaidHours((2700000), totalHours);
+      console.log('SECONDS ')
+    }
+
+    else if(shiftStar.getHours() <= 11  && shiftEnd.getHours() >= 19) {
+      this.AddPaidHours((1800000 * 2), totalHours);
+    }
+
+    else if (parseInt(+totalHours[0]) > 9 && parseInt(+totalHours[1]) >= 31){
+      
+      this.AddPaidHours((1800000 * 2), totalHours);
+      
+    }
+
+    else if (parseInt(+totalHours[0]) > 4 && parseInt(+totalHours[1]) >= 1) {
+      this.AddPaidHours((1800000), totalHours);
+    }
+
+    else if (parseInt(+totalHours[0]) > 5 && parseInt(+totalHours[1]) >= 0) {
+      this.AddPaidHours((1800000), totalHours);
+    }
+
+    else {
+      this.AddPaidHours((0), totalHours);
+    }
+
+    this.NYBreakRules(totalHours);
+
+  }
+
 
   TimeToMilliseconds(totalmilliseconds) {
 
@@ -165,43 +211,74 @@ export class BreakCalculator{
     const breakDiv = document.createElement('div');
     breakDiv.classList = `break ${breakType}`;
 
-    if(breakType === 'break-one') {
+    if(breakType === 'not-allowed') {
+      shiftBar.innerHTML = ` <div class="break break-one"><div>Not Allowed</div></div> `;
+    }
+    else if(breakType === 'break-one') {
       shiftBar.innerHTML = ` <div class="break break-one"><div>break 1</div></div> `;
     } else if (breakType === 'lunch'){
       shiftBar.innerHTML = ` 
       <div class="break break-one"><div>Break 1</div></div> 
-      <div class="break lunch"><div>Lunch</div></div> 
+      <div class="break lunch"><div>30m</div></div> 
       `;
     } else if (breakType === 'lunch-cr'){
       shiftBar.innerHTML = ` 
-      <div class="break lunch"><div>Lunch CR</div></div> 
+      <div class="break lunch"><div>30m CR</div></div> 
       `;
     } else if (breakType === 'break-two-cr'){
       shiftBar.innerHTML = ` 
       <div class="break break-one"><div>Break 1</div></div> 
-      <div class="break lunch"><div>Lunch CR</div></div> 
+      <div class="break lunch"><div>30m CR</div></div> 
       <div class="break break-two"><div>Break 2</div></div> 
       `;
     } else if (breakType === 'break-two'){
       shiftBar.innerHTML = ` 
       <div class="break break-one"><div>Break 1</div></div> 
-      <div class="break lunch"><div>Lunch</div></div> 
+      <div class="break lunch"><div>30m</div></div> 
       <div class="break break-two"><div>Break 2</div></div> 
       `;
     } else if (breakType === 'break-three-cr'){
       shiftBar.innerHTML = ` 
       <div class="break break-one"><div>Break 1</div></div> 
-      <div class="break lunch"><div>Lunch CR</div></div> 
+      <div class="break lunch"><div>30m CR</div></div> 
       <div class="break break-two"><div>Break 2</div></div> 
       <div class="break break-three"><div>Break 3</div></div> 
       `;
     } else if (breakType === 'break-three'){
       shiftBar.innerHTML = ` 
       <div class="break break-one"><div>Break 1</div></div> 
-      <div class="break lunch"><div>Lunch</div></div> 
-      <div class="break lunch-two"><div>2nd Lunch</div></div> 
+      <div class="break lunch"><div>30m</div></div> 
+      <div class="break lunch-two"><div>30m</div></div> 
       <div class="break break-two"><div>Break 2</div></div> 
       <div class="break break-three"><div>Break 3</div></div> 
+      `;
+    } else if (breakType === 'break-ny') {
+      shiftBar.innerHTML = ` 
+      <div class="break break-one"><div>Break 1</div></div> 
+      <div class="break lunch"><div>30m NY</div></div> 
+      <div class="break break-two"><div>Break 2</div></div> 
+      `; 
+    }
+    else if (breakType === 'break-ny-secondLunch') {
+      shiftBar.innerHTML = ` 
+      <div class="break break-one"><div>Break 1</div></div> 
+      <div class="break lunch"><div>30m</div></div> 
+      <div class="break lunch-two"><div>30m</div></div> 
+      <div class="break break-two"><div>Break 2</div></div> 
+      `;
+    } else if (breakType === 'break-ny-45') {
+      shiftBar.innerHTML = ` 
+      <div class="break break-one"><div>Break 1</div></div>
+      <div class="break lunch"><div>45m NY</div></div>
+      <div class="break break-two"><div>Break 2</div></div>
+      `;
+    } else if (breakType === 'break-ny-45-10hours') {
+      shiftBar.innerHTML = ` 
+      <div class="break break-one"><div>Break 1</div></div>
+      <div class="break lunch"><div>45m NY</div></div> 
+      <div class="break lunch"><div>30m NY</div></div>
+      <div class="break break-two"><div>Break 2</div></div>
+      <div class="break break-three"><div>Break 3</div></div>
       `;
     }
      else {
@@ -213,7 +290,7 @@ export class BreakCalculator{
   CRbreakRules(workTime){
 
     if((workTime[0] > 10 && workTime[1] >= 1) || (workTime[0] > 11 && workTime[1] == 0)){
-
+      this.AddBreakToUI('not-allowed');
       this.ShowMessages('CR daily limit is 11:00 hours');
       
     } else if (workTime[0] >= 9) {
@@ -237,10 +314,56 @@ export class BreakCalculator{
     }
   }
 
+  NYBreakRules(workTime) {
+    const shiftStar = new Date(this.startTime.value);
+    const shiftEnd = new Date(this.endTime.value);
+
+    if((workTime[0] > 11 && workTime[1] > 30) || (workTime[0] > 12 && workTime[1] >= 0)){
+      this.ShowMessages('US daily limit is 12 hours. <br>11 hours is the limit for California and Mexico');
+      this.AddBreakToUI('not-allowed');
+      
+    } else if ((shiftStar.getHours() < 11  && shiftEnd.getHours() >= 19) && workTime[0] > 5){
+
+      this.ShowMessages("Add second 30-minutes lunch between 5 PM and 7 PM");
+      this.AddBreakToUI('break-ny-secondLunch');
+
+    } else if (((shiftStar.getHours() > 12 && shiftEnd.getHours() <= 23 ) || (shiftStar.getHours() > 12 && shiftEnd.getHours() < 7 ) || shiftStar.getHours() <= 6  ) && workTime[0] > 9 && workTime[1] >= 31 ) {
+
+      this.ShowMessages("Add a 45-minutes lunch and a second 30-minutes lunch + breaks");
+      this.AddBreakToUI('break-ny-45-10hours');
+
+    } else if ((shiftStar.getHours() > 12 && shiftEnd.getHours() <= 23 ) && workTime[0] > 5 || (shiftStar.getHours() >= 0 && shiftEnd.getHours() < 7 ) && workTime[0] > 5 || ( shiftStar.getHours() <= 6 && shiftStar.getMinutes() < 1 ) && workTime[0] > 5) {
+      this.ShowMessages("Add a 45-minutes lunch + breaks for people who work between 1:00 PM and 6:00 AM");
+      this.AddBreakToUI('break-ny-45');
+    }
+
+    else if ( (  (shiftStar.getHours() > 8 && shiftStar.getHours() < 11 && shiftStar.getMinutes() >= 30) || (shiftStar.getHours() > 10 && shiftStar.getHours() < 12 && shiftStar.getMinutes() < 1 ) ) && workTime[0] > 5 )  {
+
+      this.ShowMessages("Add lunch between 11:00 AM and 2:00 PM. <br> Add the lunch and 2 breaks");
+      this.AddBreakToUI('break-ny');
+
+    } 
+    
+    else {
+      this.USMXbreakRules(workTime);
+    }
+
+  }
+
+  MXBreakRules(workTime){
+    
+    if((workTime[0] > 10 && workTime[1] >= 1) || (workTime[0] > 11 && workTime[1] == 0)){
+      this.AddBreakToUI('not-allowed');
+      this.ShowMessages('MX daily limit is 11:00 hours');
+      
+    } 
+  }
+
   USMXbreakRules(workTime) {
 
     if((workTime[0] > 11 && workTime[1] > 30) || (workTime[0] > 12 && workTime[1] >= 0)){
-      this.ShowMessages('US and MX daily limit is 12 hours. <br>11 hours is the limit for California');
+      this.ShowMessages('US daily limit is 12 hours. <br>11 hours is the limit for California');
+      this.AddBreakToUI('not-allowed');
       
     } else if( (workTime[0] > 9 && workTime[1] > 30) || (workTime[0] > 10 && workTime[1] >= 0 )) {
       this.AddBreakToUI('break-three');
